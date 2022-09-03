0f4c93608760c5cd4e24756612439e5c63366ab9
abduljabbar15 <abduljabber1532002@gmail.com>
2022-09-03 22:45:02 +0600

    d

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const userRoute = require('./Routes/user.route')
 const server = express()
-const PORT = 3000
+const PORT = process.env.PORT|| 3000
 
 server.use(express.json())
 server.use('/user', userRoute)
